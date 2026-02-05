548ae5dddc667e6fdbb900a55dbe5a353714c30c
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2026-02-04 23:37:22 -0300

    Actualiza config, SDK y navegación a política privacidad
    Se actualizan RootNamespace y títulos en varios .csproj para mayor coherencia. Se eleva minSdkVersion/targetSdkVersion de Android a 25/36 y se añaden permisos explícitos en AndroidManifest.xml. Se incorpora acceso a la política de privacidad desde el menú usando Browser.Default.OpenAsync. Se eliminan archivos y configuraciones de iOS/MacCatalyst en proyectos que ya no los soportan, simplificando la multiplataforma. También se eliminan carpetas y referencias innecesarias.

--- Ejemplos_Maui_Comenzando/Ejemplo_Flyout/AppShell.xaml.cs
+++ Ejemplos_Maui_Comenzando/Ejemplo_Flyout/AppShell.xaml.cs
@@ -1,9 +1,38 @@
-﻿namespace Ejemplo_Flyout;
+﻿using Java.Net;
+using System.Windows.Input;
+using Xamarin.Google.Crypto.Tink.Shaded.Protobuf;
+using static Android.Provider.Settings;
+
+namespace Ejemplo_Flyout;
 
 public partial class AppShell : Shell
 {
+
+    public ICommand ContactCommand { get; private set; }
+
     public AppShell()
     {
         InitializeComponent();
+
+        ContactCommand = new Command<string>(async (url) =>
+        {
+            if (!string.IsNullOrEmpty(url))
+            {
+                await Browser.Default.OpenAsync(new Uri(url), BrowserLaunchMode.SystemPreferred);
+            }
+        });
+        BindingContext = this;
+    }
+
+    async private void MenuItem_Clicked(object sender, EventArgs e)
+    {
+        string url="https://www.frp.utn.edu.ar/info2/?page_id=18452";
+
+        //SystemPreferred:  Abre la URL en el navegador predeterminado del sistema.Es la opción recomendada 
+        //        porque respeta la preferencia del usuario sobre qué navegador utilizar.
+
+        //External: Abre la URL en una aplicación externa al navegador.Es más antiguo y generalmente
+        //equivalente a SystemPreferred en la mayoría de casos.
+        await Browser.Default.OpenAsync(new Uri(url), BrowserLaunchMode.SystemPreferred);
     }
 }
