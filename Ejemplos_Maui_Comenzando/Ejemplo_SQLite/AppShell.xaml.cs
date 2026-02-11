20c410032e21e3a8206b8379c875becf96649243
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2026-02-11 08:57:35 -0300

    avanzando con sqlite

---
+++ Ejemplos_Maui_Comenzando/Ejemplo_SQLite/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Ejemplo_SQLite
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Ejemplos_Maui_Comenzando/Ejemplo_SQLite/MauiProgram.cs
@@ -0,0 +1,35 @@
+﻿using Ejemplo_SQLite.Data;
+using Microsoft.Extensions.Logging;
+
+namespace Ejemplo_SQLite;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+
+
+    static MauiAppBuilder AddServides(this MauiAppBuilder builder)
+    {
+        builder.Services.AddSingleton<TodoListPage>();
+        builder.Services.AddTransient<TodoItemPage>();
+
+        builder.Services.AddSingleton<TodoItemDatabase>();
+        return builder;
+    }
+}
