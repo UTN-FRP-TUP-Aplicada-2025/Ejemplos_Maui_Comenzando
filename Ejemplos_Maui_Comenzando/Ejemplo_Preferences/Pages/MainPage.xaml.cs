571b1f401120ff37e3d2f75f478a163eb2882cdd
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2026-02-27 15:26:26 -0300

    primera prueba print thermal

---
+++ Ejemplos_Maui_Comenzando/Ejemplo_Preferences/Pages/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace Ejemplo_Preferences.Pages;
+
+public partial class MainPage : ContentPage
+{
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnSetClicked(object? sender, EventArgs e)
+    {
+        Preferences.Default.Set("texto", etTexto.Text);
+    }
+
+    private void OnGetClicked(object? sender, EventArgs e)
+    {
+        etTexto.Text = Preferences.Default.Get("texto","");
+    }
+}
---
+++ Ejemplos_Maui_Comenzando/Ejemplo_Preferences/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Ejemplo_Preferences
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
