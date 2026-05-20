105acf9997d24ae07459b76584edfcb53fad714b
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2026-05-19 22:02:04 -0300

    secrets

--- Ejemplos_Maui_Comenzando/Hibridas/Ejemplo_WebView/Pages/MainPage.xaml.cs
+++ Ejemplos_Maui_Comenzando/Hibridas/Ejemplo_WebView/Pages/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿namespace Ejemplo_WebView.Pages;
+﻿
+namespace Ejemplo_WebView.Pages;
 
 public partial class MainPage : ContentPage
 {
@@ -58,19 +59,39 @@ public partial class MainPage : ContentPage
 
         BindingContext = this;
 
-        webView.Source = "https://geometriafernando.somee.com/";
+        //webView.Source = "https://geometriafernando.somee.com/";
+        webView.Source = "https://www.google.com/";
     }
 
     private void WebView_Navigating(object sender, WebNavigatingEventArgs e)
     {
+        //invisibiliza al dialer
+        IsRefreshing = false;
     }
 
     private void WebView_Navigated(object sender, WebNavigatedEventArgs e)
     {
     }
 
+    //protected override void OnAppearing()
+    //{
+
+    //    // muestra el dialer
+    //    // activa el refreshing
+        
+    //}
+
+    protected override async void OnAppearing()
+    {
+        IsRefreshing = true;
+
+        base.OnAppearing();
+    }
+
     private void OnRefreshViewRefreshing(object sender, EventArgs e)
     {
+        
+
         if (!EnableRefreshing)
         {
             IsRefreshing = false;
@@ -78,6 +99,11 @@ public partial class MainPage : ContentPage
         }
 
         webView.Reload();
+    }
+
+    private void OnPrueba(object sender, EventArgs e)
+    {
+        IsRefreshing = true;
     }
 
 }
